f4b79091103107f53b00df9db9ab932cb3c18acc
dlgilligan <danny.gilligan26@gmail.com>
2025-03-26 01:11:00 -0400

    Task 2

---
+++ practice-exam/2/adhoc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ansible all -m group -a "name=automation state=present" -u SUDOER -b -k
+ansible all -m user -a "name=automation \
+	state=present \
+	group=wheel \
+	groups=wheel \
+	append=yes" -u SUDOER -b -k
+ansible all -m file -a "path=/home/automation/.ssh \
+	state=directory \
+	owner=automation \
+	group=automation \
+	mode=0700" -u SUDOER -b -k
+ansible all -m lineinfile -a "path=/home/automation/.ssh/authorized_keys \
+	owner=automation \
+	group=automation \
+	mode=0600 \
+	create=yes \
+	line=" -u SUDOER -b -k
+ansible all -m lineinfile -a "path=/etc/sudoers \
+	regexp='^%wheel ALL=(ALL)' \
+	line='%wheel ALL=(ALL) NOPASSWD: ALL' \
+	validate='/usr/sbin/visduo -c'" -u vagrant -b -k
